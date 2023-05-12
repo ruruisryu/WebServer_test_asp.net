6a40f104ffc96c09e6d1fb0cbccbb923aa88327f
ruruisryu <atz999@ewhain.net>
2023-05-13 01:03:54 +0900

    async_await (2)

--- Csharp/Csharp/async_await.cs
+++ Csharp/Csharp/async_await.cs
@@ -11,20 +11,25 @@ namespace Study
             return t;
         }
 
-        static async void TestAsync()
+        // await을 사용하기 위해서는 함수에 async를 선언해주어야한다.
+        // 반환형은 항상 Task
+        static async Task TestAsync()
         {
             Console.WriteLine("Start TestAsync");
-            Task t = Task.Delay(3000);
-            await t;
+            // Task가 완료된 후에ㅣ End TestAsync 출력
+            // 하지만 외부에서 Task가 완료될 때까지 기다리지는 않는다.
+            await Task.Delay(3000);
             Console.WriteLine("End TestAsync");
         }
         
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             //Task t = Test();
             //t.Wait();
             
-            TestAsync();
+            //이부분의 await는 Main에 대해 대기를 걸어버림.
+            // 그러므로 TestAsync()가 완료될 때까지 그 아래 스크립트 실행하지 못함.
+            await TestAsync();
 
             Console.WriteLine("While start");
             while(true){}
