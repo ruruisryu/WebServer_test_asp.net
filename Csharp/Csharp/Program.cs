862ecfa355c3e3baac2bb459d68bdb329471b785
ruruisryu <79790536+ruruisryu@users.noreply.github.com>
2023-05-13 00:43:16 +0900

    Test

---
+++ Csharp/Csharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Study
+{
+    class Program
+    {
+        static Task Test()
+        {
+            Console.WriteLine("Start Test");
+            Task t = Task.Delay(3000);
+            return t;
+        }
+
+        static void TestAsync()
+        {
+            Console.WriteLine("Start TestAsync");
+            Task t = Task.Delay(3000);
+            t.Wait();
+            Console.WriteLine("End TestAsync");
+        }
+        
+        static void Main(string[] args)
+        {
+            Task t = Test();
+            t.Wait();
+            
+            Console.WriteLine("While start");
+            while(true){}
+        }
+    }
+}
